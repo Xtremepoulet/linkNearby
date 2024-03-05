59c32fe883e6304124f4465d2496eccb1401021f
multimedis <cerisier.jeremy@gmail.com>
2024-03-05 10:14:32 +0100

    Modification du css login

--- frontend/screens/FirstLoginScreen.jsx
+++ frontend/screens/FirstLoginScreen.jsx
@@ -1,31 +1,36 @@
-import { Pressable, StyleSheet, Text, View, Image, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { Pressable, StyleSheet, Text, View, Image, TextInput, KeyboardAvoidingView, Platform, SafeAreaView } from 'react-native';
 import logoLinkNearby from '../assets/linkNearbyBackNone.webp';
 
 export default function FirstLoginScreen({ navigation }) {
+
+    const Container = Platform.OS === 'ios' ? SafeAreaView : View;
+
     return (
         <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             style={styles.container}
         >
-            <View style={styles.header}>
-                <Image source={logoLinkNearby} style={styles.logo} />
-                <Text style={styles.h1}>LINK NEARBY</Text>
-            </View>
-            <View style={styles.body}>
-                <Pressable
-                    style={styles.button}
-                    title="Go to SecondLoginScreen"
-                    onPress={() => navigation.navigate('signupScreen')}
-                >
-                    <Text style={styles.text}>Créer un compte</Text>
-                </Pressable>
-                <Pressable
-                    style={styles.button}
-                    title="Go to signin screen"
-                    onPress={() => navigation.navigate('signinScreen')}
-                >
-                    <Text style={styles.text}>Connexion</Text>
-                </Pressable>
-            </View>
+            <Container style={styles.container}>
+                <View style={styles.header}>
+                    <Image source={logoLinkNearby} style={styles.logo} />
+                    <Text style={styles.h1}>LINK NEARBY</Text>
+                </View>
+                <View style={styles.body}>
+                    <Pressable
+                        style={styles.button}
+                        title="Go to SecondLoginScreen"
+                        onPress={() => navigation.navigate('signupScreen')}
+                    >
+                        <Text style={styles.text}>Créer un compte</Text>
+                    </Pressable>
+                    <Pressable
+                        style={styles.button}
+                        title="Go to signin screen"
+                        onPress={() => navigation.navigate('signinScreen')}
+                    >
+                        <Text style={styles.text}>Connexion</Text>
+                    </Pressable>
+                </View>
+            </Container>
         </KeyboardAvoidingView>
     );
 }
@@ -39,21 +44,19 @@ const styles = StyleSheet.create({
     header: {
         justifyContent: 'center',
         alignItems: 'center',
-        flexDirection: 'row',
-        marginTop: 70,
-        // backgroundColor: 'blue',
-        height: '10%',
+        marginTop: 200,
         width: '100%',
+
     },
     h1: {
-        fontSize: 40,
+        fontSize: 60,
         fontWeight: 'bold',
         color: 'black'
     },
     logo: {
-        width: 80,
-        height: 80,
-        marginRight: 10
+        width: 100,
+        height: 100,
+        marginBottom: 20
     },
     img: {
         width: '100%',
