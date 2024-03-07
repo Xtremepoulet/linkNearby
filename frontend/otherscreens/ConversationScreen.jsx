2ff080292929cfad5826e8ce52333116a5984c4c
adrian <adrian.chesneau@gmail.com>
2024-03-07 11:24:30 +0100

    conv

--- frontend/otherscreens/ConversationScreen.jsx
+++ frontend/otherscreens/ConversationScreen.jsx
@@ -7,14 +7,25 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 export default function ConversationScreen({ navigation }) {
     return (
         <SafeAreaView style={styles.container} edges={['top']} styleAndroid={{ flex: 1 }}>
+            <View style={styles.alignHeader} >
+                <Pressable style={styles.paddingArrow}
+                    onPress={() => navigation.goBack()}
+                >
+                    <FontAwesome name="arrow-left" size={24} style={styles.arrowIcon} />
+                </Pressable>
+                <Pressable style={styles.card}>
 
-            <Text style={styles.textTitle}>messages <FontAwesome name="comment" size={24} />
-            </Text>
+                    <View style={styles.nope}>
+                        <Image style={styles.image} source={require('../assets/profile.png')}></Image>
+                    </View>
+                    <View style={styles.msg}>
+                        <Text>Jerem Ptit Zbeub</Text>
+                    </View>
+                </Pressable>
+            </View>
 
-            <ScrollView style={styles.oui}>
-                <Text style={styles.textBody}>Messages</Text>
 
-            </ScrollView>
+
         </SafeAreaView>
     );
 }
@@ -24,6 +35,69 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
     },
+    oui: {
+        flex: 1,
+        width: '95%',
+        height: '20%',
+
+
+    },
+    border: {
+        borderRadius: 5,
+        height: '85%',
+        width: 300,
+        backgroundColor: 'white'
+    },
+    stretch: {
+        height: '6%',
+        width: 300,
+    },
+
+
+    textBody: {
+        fontSize: 25,
+        fontWeight: 'bold',
+    }
+    , textTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 5
+    },
+    image: {
+        height: 45,
+        width: 45,
+        padding: 20
+    },
+    card: {
+        flexDirection: 'row',
+        marginTop: 5,
+        borderRadius: 8,
+        height: 55
+    },
+    msg: {
+        flexDirection: "column",
+        justifyContent: 'space-evenly',
+        padding: 10
+    },
+
+
+    nope: {
+        padding: 6
+    },
+    alignHeader: {
+        flex: 0.09,
+
+        width: '100%',
+        flexDirection: 'row',
+        alignContent: 'center',
+        alignItems: 'center',
+        borderBottomWidth: 2,
+
+
+    },
+    paddingArrow: {
+        padding: 20
+    }
 
 })
 
