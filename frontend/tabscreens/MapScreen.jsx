3f771cc3c9774917453e88805537d5292182ed8b
yanis <layrac.yanis@gmail.com>
2024-03-07 11:21:47 +0100

    commit

--- frontend/tabscreens/MapScreen.jsx
+++ frontend/tabscreens/MapScreen.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { KeyboardAvoidingView, Image, StyleSheet, Text, View, Platform } from 'react-native';
 import Constants from 'expo-constants';
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
-    
 import {Dimensions} from 'react-native';
 import { useSelector } from 'react-redux';
+
 
 const CONNECTION_BACKEND = Constants.expoConfig?.extra?.CONNECTION_BACKEND;
 
@@ -15,7 +15,29 @@ const ProfileScreen = ({ navigation }) => {
     const latitude = useSelector((state) => state.users.value.latitude);
     const longitude = useSelector((state) => state.users.value.longitude);
 
-    console.log(longitude)
+
+    const [users_positions, setUsers_positions] = useState([]);
+
+    
+    useEffect(() => {
+            load_users_position();
+    }, [])
+
+    const load_users_position = async () => {
+        const fetching_data = await fetch(`${CONNECTION_BACKEND}/user/users_position`);
+        const result = await fetching_data.json();
+       
+        setUsers_positions(result.users);
+        
+    }
+
+
+    const users_positions_to_display = users_positions.map((user, i) => {
+        return <Marker key={i} coordinate={{ latitude: user.location[0].latitude, longitude: user.location[0].longitude}} />  
+    })
+
+    
+
 
     return (
         <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -29,7 +51,7 @@ const ProfileScreen = ({ navigation }) => {
                 }}
                 style={styles.map}
                 >
-                     <Marker coordinate={{ latitude: latitude, longitude: longitude }} />
+                    {users_positions_to_display}
                 </MapView>
                 
         </KeyboardAvoidingView>
